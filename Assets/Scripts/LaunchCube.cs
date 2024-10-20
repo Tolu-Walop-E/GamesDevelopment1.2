a6b6059e5c8f436ced6ae88ef2e3f23d425e897d
Tolu Adesina <adesinatolu8@gmail.com>
2024-10-20 19:22:12 +0100

    enemy looks like player and shoots

--- Assets/Scripts/LaunchCube.cs
+++ Assets/Scripts/LaunchCube.cs
@@ -4,32 +4,43 @@ using UnityEngine;
 
 public class LaunchCube : MonoBehaviour
 {
-    public float bounceForce = 15f;
-    public string playerTag = "Player";
+    public float bounceHeight = 5f;  // The height to which the player will be launched
+    public string playerTag = "Player";  // Tag to identify the player object
+    public float fallSpeed = 9.8f;  // Simulated gravity for the player falling back down
+
+    private Transform playerTransform;
+    private bool isLaunching = false;
+    private float launchSpeed = 0f;
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag(playerTag))
         {
-            Rigidbody playerRigidBody = collision.collider.GetComponent<Rigidbody>();
+            // Get the player's transform
+            playerTransform = collision.collider.transform;
 
-            if (playerRigidBody != null)
+            // Set launch speed based on bounce height
+            launchSpeed = Mathf.Sqrt(2 * fallSpeed * bounceHeight);  // Calculate velocity needed to reach the bounce height
+            isLaunching = true;  // Start launching the player
+        }
+    }
+
+    void Update()
+    {
+        // If player is being launched, move them upward
+        if (isLaunching && playerTransform != null)
+        {
+            // Move the player upwards
+            playerTransform.position += new Vector3(0, launchSpeed * Time.deltaTime, 0);
+
+            // Simulate gravity (reduce launch speed over time to simulate the player coming back down)
+            launchSpeed -= fallSpeed * Time.deltaTime;
+
+            // Stop launching when launchSpeed is below zero (falling back to the ground)
+            if (launchSpeed <= 0f)
             {
-                Vector3 launchDirection = new Vector3(0, bounceForce, 0);
-                playerRigidBody.AddForce(launchDirection, ForceMode.Impulse);
+                isLaunching = false;
             }
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
-
